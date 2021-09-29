b18a8c18a4acf646b816cc7e9da1653ef5267c06
sshrik <chj878194@naver.com>
2021-09-29 14:53:01 +0900

    ts 설정.

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+
+function App(): ReactElement {
+  return (
+    <div>Hello World!</div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const join = require('path').join;
+
+module.exports = {
+  mode: 'development',
+  entry: join(__dirname, '/src/index.tsx'),
+  devtool: 'eval-cheap-source-map',
+  output: {
+    filename: "main.js",
+    path: join(__dirname, '/dist'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.tsx', '.ts']
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          "presets": ["@babel/preset-env", "@babel/preset-react"]
+        }
+      }
+    },
+    {
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/,
+    },
+    {
+      test: /\.(scss|css)$/,
+      use: ["css-loader"]
+    }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: join(__dirname, './public/index.html')
+    })
+  ],
+};
